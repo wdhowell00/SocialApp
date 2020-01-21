a803445cd9c9431befc17b603883acf3914f8895
William Howell <whowell@mente-tattica.com>
2020-01-21 09:52:32 -0500

    update references and add initial db context

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
