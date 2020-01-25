2d0bc31e6c16de36d3dcad6a378ae0942c85d929
William Howell <whowell@mente-tattica.com>
2020-01-25 13:46:27 -0500

    Add ACtivity CRUD handlers

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace Persistence
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
